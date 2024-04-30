8d70785a20538c27ed0f0ad3f40454a251d96776
Gustavo Dias <gustavogdcosta@hotmail.com>
2024-04-30 08:11:24 -0300

    ajustando pagina restaurante

--- src/components/Projeto/13-Restaurante/index.tsx
+++ src/components/Projeto/13-Restaurante/index.tsx
@@ -6,7 +6,7 @@ const ProjetoTreza = () => (
     <Card>
         <Titulo> Pagina Restaurante </Titulo>
         <Paragrafo tipo="secundario">
-        Este projeto envolve a criação de uma página web dedicada a um restaurante, utilizando o framework Bootstrap. O objetivo é desenvolver uma interface atrativa e funcional que 
+        Este projeto envolve a criação de uma página de um restaurante, utilizando o Bootstrap. O objetivo é desenvolver uma interface atrativa que 
         destaque os pratos, serviços e ambiente oferecidos pelo restaurante, proporcionando aos visitantes uma experiência online que os incentive a visitar o local.
         </Paragrafo>
         <LinkBotao href="https://github.com/Gustavogdcosta/modulo14-exemplo" target="_blank"> Visualizar Código </LinkBotao>
